89fae5466f7f7ea8f735fa1fd3e28a1ec4b9d6c7
Aref Daei <aref.daei@outlook.com>
2026-01-23 00:00:33 +0330

    feat: creat Model abstract class

---
+++ src/model/Model.java
@@ -0,0 +1,35 @@
+package model;
+
+public abstract class Model {
+    private String id;
+
+    public Model(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Model model = (Model) o;
+        return id.equals(model.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+}
