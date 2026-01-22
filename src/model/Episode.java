a50813a2be3d5541b093d493d9d3b2ab3a7cf7d1
Aref Daei <aref.daei@outlook.com>
2026-01-23 02:24:20 +0330

    fix: move calculateAffinity method from Episode class to EpisodeRecommenderService

--- src/model/Episode.java
+++ src/model/Episode.java
@@ -4,19 +4,19 @@ public class Episode extends Model {
     private String title;
     private String genre;
     private int year;
-    private ProductionTeam prod_team;
+    private ProductionTeam productionTeam;
     private String language;
     private int duration;
     private float rating;
 
     public Episode(String id, String title, String genre, int year,
-                   ProductionTeam prod_team,
+                   ProductionTeam productionTeam,
                    String language, int duration, float rating) {
         super(id);
         this.title = title;
         this.genre = genre;
         this.year = year;
-        this.prod_team = prod_team;
+        this.productionTeam = productionTeam;
         this.language = language;
         this.duration = duration;
         this.rating = rating;
@@ -46,12 +46,12 @@ public class Episode extends Model {
         this.year = year;
     }
 
-    public ProductionTeam getProd_team() {
-        return prod_team;
+    public ProductionTeam getProductionTeam() {
+        return productionTeam;
     }
 
-    public void setProd_team(ProductionTeam prod_team) {
-        this.prod_team = prod_team;
+    public void setProductionTeam(ProductionTeam productionTeam) {
+        this.productionTeam = productionTeam;
     }
 
     public String getLanguage() {
@@ -76,18 +76,5 @@ public class Episode extends Model {
 
     public void setRating(float rating) {
         this.rating = rating;
-    }
-
-    public static int calculateAffinity(Episode ep1, Episode ep2) {
-        int affinity = 0;
-
-        if (ep1.genre.equals(ep2.genre)) affinity += 6;
-        if (Math.abs(ep1.rating - ep2.rating) <= 1) affinity += 5;
-        if (ep1.prod_team.equals(ep2.prod_team)) affinity += 4;
-        if (ep1.language.equals(ep2.language)) affinity += 3;
-        if (Math.abs(ep1.year - ep2.year) <= 10) affinity += 2;
-        if (Math.abs(ep1.duration - ep2.duration) <= 30) affinity += 1;
-
-        return affinity;
     }
 }
--- src/service/EpisodeRecommenderService.java
+++ src/service/EpisodeRecommenderService.java
@@ -1,4 +1,18 @@
 package service;
 
+import model.Episode;
+
 public class EpisodeRecommenderService {
+    public int calculateAffinity(Episode ep1, Episode ep2) {
+        int affinity = 0;
+
+        if (ep1.getGenre().equals(ep2.getGenre())) affinity += 6;
+        if (Math.abs(ep1.getRating() - ep2.getRating()) <= 1) affinity += 5;
+        if (ep1.getProductionTeam().equals(ep2.getProductionTeam())) affinity += 4;
+        if (ep1.getLanguage().equals(ep2.getLanguage())) affinity += 3;
+        if (Math.abs(ep1.getYear() - ep2.getYear()) <= 10) affinity += 2;
+        if (Math.abs(ep1.getDuration() - ep2.getDuration()) <= 30) affinity += 1;
+
+        return affinity;
+    }
 }
