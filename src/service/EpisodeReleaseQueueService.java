988b47d23802db67194b745b773daa73862fbad6
Aref Daei <aref.daei@outlook.com>
2026-01-30 16:07:07 +0330

    feat: create EpisodeReleaseQueueService class

---
+++ src/service/EpisodeReleaseQueueService.java
@@ -0,0 +1,26 @@
+package service;
+
+import model.Episode;
+
+public class EpisodeReleaseQueueService {
+    // MinHeap not complete
+    public EpisodeReleaseQueueService() {}
+
+    public void insert(Episode episode, int priority) {
+    }
+
+    public Episode extractMin() {
+        return null;
+    }
+
+    public Episode delete(Episode episode) {
+        return null;
+    }
+
+    public String display() {
+        return null;
+    }
+
+    public void HeapSort() {
+    }
+}
