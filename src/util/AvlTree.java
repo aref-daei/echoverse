5be8ecc03d3909f5561f50c36f36b5750ffc82a1
Aref Daei <aref.daei@outlook.com>
2026-02-03 02:56:24 +0330

    fix: rewrite AvlTree class and improve its performance

--- src/util/AvlTree.java
+++ src/util/AvlTree.java
@@ -4,38 +4,84 @@ import model.Model;
 
 public class AvlTree<E extends Model> extends LinkedBinaryTree<E> {
 
-    private int height(TNode<E> n) {
-        return n == null ? 0 : ((AvlNode<E>) n).height;
+    protected static class AvlNode<E> extends Node<E> {
+        int height;
+
+        AvlNode(E element, Node<E> parent) {
+            super(element, parent);
+            this.height = 1;
+        }
     }
 
-    private void updateHeight(TNode<E> n) {
-        ((AvlNode<E>) n).height =
-                1 + Math.max(height(n.left), height(n.right));
+    protected AvlNode<E> insert(AvlNode<E> node, E element) {
+        if (node == null) {
+            size++;
+            return new AvlNode<>(element, null);
+        }
+
+        if (element.compareTo(node.element) < 0) {
+            node.left = insert((AvlNode<E>) node.left, element);
+            node.left.parent = node;
+        } else if (element.compareTo(node.element) > 0) {
+            node.right = insert((AvlNode<E>) node.right, element);
+            node.right.parent = node;
+        } else {
+            return node;
+        }
+
+        updateHeight(node);
+        return balance(node);
     }
 
-    private int balance(TNode<E> n) {
-        return height(n.left) - height(n.right);
+    protected AvlNode<E> delete(AvlNode<E> node, E element) {
+        if (node == null) return null;
+
+        if (element.compareTo(node.element) < 0) {
+            node.left = delete((AvlNode<E>) node.left, element);
+        } else if (element.compareTo(node.element) > 0) {
+            node.right = delete((AvlNode<E>) node.right, element);
+        } else {
+            size--;
+            if (node.left == null || node.right == null) {
+                AvlNode<E> temp =
+                        node.left != null ? (AvlNode<E>) node.left : (AvlNode<E>) node.right;
+                if (temp == null) return null;
+                temp.parent = node.parent;
+                return temp;
+            } else {
+                AvlNode<E> successor = min((AvlNode<E>) node.right);
+                node.element = successor.element;
+                node.right = delete((AvlNode<E>) node.right, successor.element);
+            }
+        }
+
+        updateHeight(node);
+        return balance(node);
     }
 
-    private int compare(E a, E b) {
-        return a.getId().compareTo(b.getId());
+    protected AvlNode<E> balance(AvlNode<E> node) {
+        int bf = balanceFactor(node);
+
+        if (bf > 1) {
+            if (balanceFactor((AvlNode<E>) node.left) < 0)
+                node.left = rotateLeft((AvlNode<E>) node.left);
+            return rotateRight(node);
+        }
+
+        if (bf < -1) {
+            if (balanceFactor((AvlNode<E>) node.right) > 0)
+                node.right = rotateRight((AvlNode<E>) node.right);
+            return rotateLeft(node);
+        }
+        return node;
     }
 
-    private TNode<E> rotateRight(TNode<E> y) {
-        TNode<E> x = y.left;
-        TNode<E> t2 = x.right;
+    protected AvlNode<E> rotateRight(AvlNode<E> y) {
+        AvlNode<E> x = (AvlNode<E>) y.left;
+        AvlNode<E> t2 = (AvlNode<E>) x.right;
 
         x.right = y;
         y.left = t2;
-
-        if (t2 != null) t2.parent = y;
-
-        x.parent = y.parent;
-        y.parent = x;
-
-        if (x.parent == null) root = x;
-        else if (x.parent.left == y) x.parent.left = x;
-        else x.parent.right = x;
 
         updateHeight(y);
         updateHeight(x);
@@ -43,21 +89,12 @@ public class AvlTree<E extends Model> extends LinkedBinaryTree<E> {
         return x;
     }
 
-    private TNode<E> rotateLeft(TNode<E> x) {
-        TNode<E> y = x.right;
-        TNode<E> t2 = y.left;
+    protected AvlNode<E> rotateLeft(AvlNode<E> x) {
+        AvlNode<E> y = (AvlNode<E>) x.right;
+        AvlNode<E> t2 = (AvlNode<E>) y.left;
 
         y.left = x;
         x.right = t2;
-
-        if (t2 != null) t2.parent = x;
-
-        y.parent = x.parent;
-        x.parent = y;
-
-        if (y.parent == null) root = y;
-        else if (y.parent.left == x) y.parent.left = y;
-        else y.parent.right = y;
 
         updateHeight(x);
         updateHeight(y);
@@ -65,112 +102,48 @@ public class AvlTree<E extends Model> extends LinkedBinaryTree<E> {
         return y;
     }
 
-    public void insert(E e) {
-        root = insert(root, e, null);
+    protected void updateHeight(AvlNode<E> n) {
+        n.height = Math.max(height(n.left), height(n.right)) + 1;
     }
 
-    private TNode<E> insert(TNode<E> node, E e, TNode<E> parent) {
-        if (node == null) {
-            size++;
-            return new AvlNode<>(e, parent);
-        }
-
-        int cmp = compare(e, node.element);
-
-        if (cmp < 0)
-            node.left = insert(node.left, e, node);
-        else if (cmp > 0)
-            node.right = insert(node.right, e, node);
-        else
-            return node; // duplicate id
-
-        updateHeight(node);
-        return rebalance(node);
+    protected int height(Node<E> n) {
+        return n == null ? 0 : ((AvlNode<E>) n).height;
     }
 
-    public void delete(E e) {
-        root = delete(root, e.getId());
+    protected int balanceFactor(AvlNode<E> n) {
+        return height(n.left) - height(n.right);
     }
 
-    private TNode<E> delete(TNode<E> node, String id) {
-        if (node == null) return null;
-
-        int cmp = id.compareTo(node.element.getId());
-
-        if (cmp < 0)
-            node.left = delete(node.left, id);
-        else if (cmp > 0)
-            node.right = delete(node.right, id);
-        else {
-            size--;
-
-            if (node.left == null || node.right == null) {
-                TNode<E> temp = (node.left != null) ? node.left : node.right;
-                if (temp != null) temp.parent = node.parent;
-                return temp;
-            }
-
-            TNode<E> successor = min(node.right);
-            node.element = successor.element;
-            node.right = delete(node.right, successor.element.getId());
-        }
-
-        updateHeight(node);
-        return rebalance(node);
-    }
-
-    private TNode<E> min(TNode<E> n) {
-        while (n.left != null) n = n.left;
+    protected AvlNode<E> min(AvlNode<E> n) {
+        while (n.left != null) n = (AvlNode<E>) n.left;
         return n;
     }
 
-    private TNode<E> rebalance(TNode<E> node) {
-        int b = balance(node);
-
-        // LL
-        if (b > 1 && balance(node.left) >= 0)
-            return rotateRight(node);
-
-        // LR
-        if (b > 1 && balance(node.left) < 0) {
-            node.left = rotateLeft(node.left);
-            return rotateRight(node);
+    public void add(E element) {
+        if (root == null) {
+            root = new AvlNode<>(element, null);
+            size = 1;
+            return;
         }
-
-        // RR
-        if (b < -1 && balance(node.right) <= 0)
-            return rotateLeft(node);
-
-        // RL
-        if (b < -1 && balance(node.right) > 0) {
-            node.right = rotateRight(node.right);
-            return rotateLeft(node);
-        }
-
-        return node;
-    }
-
-    public E search(String id) {
-        TNode<E> n = root;
-        while (n != null) {
-            int cmp = id.compareTo(n.element.getId());
-            if (cmp == 0) return n.element;
-            n = (cmp < 0) ? n.left : n.right;
-        }
-        return null;
+        root = insert((AvlNode<E>) root, element);
     }
 
     @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        inorder(root, sb);
-        return sb.toString().trim();
+    public E remove(E element) {
+        root = delete((AvlNode<E>) root, element);
+        return root.element;
     }
 
-    private void inorder(TNode<E> n, StringBuilder sb) {
-        if (n == null) return;
-        inorder(n.left, sb);
-        sb.append(n.element).append(" ");
-        inorder(n.right, sb);
+    public E findById(String id) {
+        AvlNode<E> current = (AvlNode<E>) root;
+        while (current != null) {
+
+            if (id.compareTo(current.element.getId()) == 0)
+                return current.element;
+            current = id.compareTo(current.element.getId()) < 0
+                    ? (AvlNode<E>) current.left
+                    : (AvlNode<E>) current.right;
+        }
+        return null;
     }
 }
