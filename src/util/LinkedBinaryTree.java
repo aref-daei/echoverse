acfb3652efa6e589433012892fd66b8e3c5753a4
Aref Daei <aref.daei@outlook.com>
2026-02-02 16:30:46 +0330

    fix: add TNode to LinkedBinaryTree class and rename TNode class to Node class

--- src/util/LinkedBinaryTree.java
+++ src/util/LinkedBinaryTree.java
@@ -1,9 +1,22 @@
 package util;
 
 public class LinkedBinaryTree<E> {
+    public static class Node<E> {
+        public E element;
+        public Node<E> left;
+        public Node<E> right;
+        public Node<E> parent;
 
-    protected TNode<E> root;
-    protected int size;
+        public Node(E element, Node<E> parent) {
+            this.element = element;
+            this.parent = parent;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    protected Node<E> root;
+    private int size;
 
     public LinkedBinaryTree() {
         root = null;
@@ -18,31 +31,31 @@ public class LinkedBinaryTree<E> {
         return size == 0;
     }
 
-    public TNode<E> root() {
+    public Node<E> root() {
         return root;
     }
 
-    public TNode<E> parent(TNode<E> node) {
+    public Node<E> parent(Node<E> node) {
         return node.parent;
     }
 
-    public TNode<E> left(TNode<E> node) {
+    public Node<E> left(Node<E> node) {
         return node.left;
     }
 
-    public TNode<E> right(TNode<E> node) {
+    public Node<E> right(Node<E> node) {
         return node.right;
     }
 
-    public boolean isInternal(TNode<E> node) {
+    public boolean isInternal(Node<E> node) {
         return node.left != null || node.right != null;
     }
 
-    public boolean isExternal(TNode<E> node) {
+    public boolean isExternal(Node<E> node) {
         return node.left == null && node.right == null;
     }
 
-    public boolean isRoot(TNode<E> node) {
+    public boolean isRoot(Node<E> node) {
         return node == root;
     }
 
@@ -50,27 +63,27 @@ public class LinkedBinaryTree<E> {
         if (!isEmpty())
             throw new IllegalStateException("Tree already has a root");
 
-        root = new TNode<>(element, null);
+        root = new Node<>(element, null);
         size = 1;
     }
 
-    public void addLeft(TNode<E> parent, E element) {
+    public void addLeft(Node<E> parent, E element) {
         if (parent.left != null)
             throw new IllegalArgumentException("Left child already exists");
 
-        parent.left = new TNode<>(element, parent);
+        parent.left = new Node<>(element, parent);
         size++;
     }
 
-    public void addRight(TNode<E> parent, E element) {
+    public void addRight(Node<E> parent, E element) {
         if (parent.right != null)
             throw new IllegalArgumentException("Right child already exists");
 
-        parent.right = new TNode<>(element, parent);
+        parent.right = new Node<>(element, parent);
         size++;
     }
 
-    public E set(TNode<E> node, E element) {
+    public E set(Node<E> node, E element) {
         E old = node.element;
         node.element = element;
         return old;
