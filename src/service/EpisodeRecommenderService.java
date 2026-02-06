1a4b8d6821ffb05fe88135bbf5a1deea418cd68b
Aref Daei <aref.daei@outlook.com>
2026-02-07 00:45:07 +0330

    fix: modify calculate affinity method

--- src/service/EpisodeRecommenderService.java
+++ src/service/EpisodeRecommenderService.java
@@ -53,9 +53,7 @@ public class EpisodeRecommenderService {
         if (ep1.equals(ep2)) return affinity;
         if (ep1.getGenre().equals(ep2.getGenre())) affinity += 6;
         if (ep1.getProductionTeam().equals(ep2.getProductionTeam())) affinity += 4;
-        if (ep1.getLanguage().equals(ep2.getLanguage())) affinity += 3;
-        if (Math.abs(ep1.getYear() - ep2.getYear()) <= 10) affinity += 2;
-        if (Math.abs(ep1.getDuration() - ep2.getDuration()) <= 30) affinity += 1;
+        if (ep1.getLanguage().equals(ep2.getLanguage())) affinity += 2;
 
         return affinity;
     }
